f1cb5548921778ed304f684105c0b00a55dd03c9
Star <67430892+alicenstar@users.noreply.github.com>
2021-02-09 15:51:16 -0600

    Fix formatting

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,6 +1,5 @@
 import {
     Box,
-    Container,
     Grid,
     Typography,
     useMediaQuery,
@@ -16,86 +15,45 @@ export const LandingPage: React.FC = () => {
     const smScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
+        <Grid
+         container
+         direction="column"
+         justify='center'
+         alignItems="center"
+         style={{ minHeight: '90vh', width: '100vw' }}
+        >
             <Grid
              container
-             direction="column"
-             justify='center'
+             item
+             direction={smScreen ? "column" : "row"}
+             justify="center"
              alignItems="center"
-             style={{ minHeight: '90vh', width: '100vw' }}
+             spacing={smScreen ? 3 : 7}
+             style={{ width: '100vw'}}
             >
-                <Grid
-                 container
-                 item
-                 direction={smScreen ? "column" : "row"}
-                 justify="center"
-                 alignItems="center"
-                 spacing={smScreen ? 3 : 7}
-                 style={{ width: '100vw'}}
-                >
-                    <Grid item xs={12}>
-                        <Typography variant={smScreen ? "h2" : "h1"} align="center">
-                            <Box letterSpacing={3}>
-                                Coin Tracker
-                            </Box>
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <CreateTracker />
-                    </Grid>
-                    <Grid item>
-                        <Typography
-                         variant="subtitle1"
-                         align="center"
-                         display="block"
-                        >
-                            OR
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <CreateAccount />
-                    </Grid>
-                </Grid>
-            </Grid>
-    );
-};
-
-{/* <Container maxWidth="md">
-            <Grid
-            container
-            direction="column"
-            justify="center"
-            alignItems="center"
-            style={{ minHeight: '90vh' }}
-            spacing={3}
-            >
-                <Grid item>
-                    <Typography variant="h3" align="center">
+                <Grid item xs={12}>
+                    <Typography variant={smScreen ? "h2" : "h1"} align="center">
                         <Box letterSpacing={3}>
                             Coin Tracker
                         </Box>
                     </Typography>
                 </Grid>
-                <Grid
-                 container
-                 item
-                 direction="row"
-                 justify="center"
-                 alignItems="center"
-                 spacing={7}
-                >
-                    <Grid item>
-                        <CreateTracker />
-                    </Grid>
-                    <Grid item>
-                        <Typography
-                         variant="subtitle1"
-                         align="center">
-                            OR
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <CreateAccount />
-                    </Grid>
+                <Grid item>
+                    <CreateTracker />
+                </Grid>
+                <Grid item>
+                    <Typography
+                     variant="subtitle1"
+                     align="center"
+                     display="block"
+                    >
+                        OR
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <CreateAccount />
                 </Grid>
             </Grid>
-        </Container> */}
+        </Grid>
+    );
+};
