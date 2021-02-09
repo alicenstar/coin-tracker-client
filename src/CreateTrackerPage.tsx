e28c57201c0fe2b86c495a6d686e7f57558d2896
Star <67430892+alicenstar@users.noreply.github.com>
2021-02-09 16:16:35 -0600

    Initial creation of CreateTrackerPage component

---
+++ src/CreateTrackerPage.tsx
@@ -0,0 +1,14 @@
+import { Grid, Typography } from '@material-ui/core';
+import React from 'react';
+
+
+export const CreateTrackerPage: React.FC = () => {
+
+    return (
+        <Grid item>
+            <Typography variant="h3">
+                Create A Tracker
+            </Typography>
+        </Grid>
+    );
+};
