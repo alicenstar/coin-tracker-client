eaceefffd4d2abaf1dfe019d184ce74b51d1f564
Star <67430892+alicenstar@users.noreply.github.com>
2021-03-04 15:07:30 -0600

    Finish implementing csv uploads

--- src/ListingsContext.tsx
+++ src/ListingsContext.tsx
@@ -33,7 +33,7 @@ export const ListingsProvider = ({
 
     useInterval(() => {
         fetchListings();
-    }, 90000);
+    }, 120000);
 
     return (
         <ListingsContext.Provider value={{ listings, setListings }}>
