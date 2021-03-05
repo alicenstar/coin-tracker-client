8e4f57a4551f24b02506ce4901663de4253909a1
Star <67430892+alicenstar@users.noreply.github.com>
2021-03-05 15:39:38 -0600

    Adjust interval, add Loading component while fetching listings

--- src/ListingsContext.tsx
+++ src/ListingsContext.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loading from './Loading';
 import { IListing } from './types/types';
 import { useInterval } from './utils';
 
@@ -20,11 +21,13 @@ export const ListingsProvider = ({
     children
 }: Props) => {
     const [ listings, setListings ] = React.useState<IListing[]>([]);
+    const [ loaded, setLoaded ] = React.useState(false);
 
     const fetchListings = React.useCallback(async () => {
         const response = await fetch('https://backend-cointracker-dev.herokuapp.com/api/listings/');
         const json = await response.json();
         setListings(json.listings);
+        setLoaded(true);
     }, []);
 
     React.useEffect(() => {
@@ -33,10 +36,11 @@ export const ListingsProvider = ({
 
     useInterval(() => {
         fetchListings();
-    }, 120000);
+    }, 300000);
 
     return (
         <ListingsContext.Provider value={{ listings, setListings }}>
+            {!loaded && <Loading />}
             {children}
         </ListingsContext.Provider>
     );
