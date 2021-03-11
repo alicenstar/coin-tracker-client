53ff8d9fdf19cf5fd8f810a47adba337161e5750
Star <67430892+alicenstar@users.noreply.github.com>
2021-03-11 12:20:24 -0600

    Change alignment of columns

--- src/OverviewTable.tsx
+++ src/OverviewTable.tsx
@@ -72,74 +72,85 @@ export const OverviewTable: React.FC<ITableProps> = ({
 
     return (
         <>
-        <TableContainer className={classes.container}>
-            <Table size="small" aria-label="a dense table">
-                {headers && (
-                    <TableHead>
-                        <TableRow>
-                            {headers.map((header: string, index: number) => {
-                                if (index === 1) {
-                                    return (
-                                        <StickyTableCell key={index}>
-                                            {header}
-                                        </StickyTableCell>
-                                    );
-                                } else {
-                                    return (
-                                        <TableCell key={index}>
-                                            {header}
-                                        </TableCell>
-                                    );
-                                }
-                            })}
-                        </TableRow>
-                    </TableHead>
-                )}
-                <TableBody>
-                    {(rowsPerPage > 0
-                        ? listings.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                        : listings).map((row: any) => (
-                        <TableRow key={row.id}>
-                            <TableCell>
-                                {row.cmc_rank}
-                            </TableCell>
-                            <StickyTableCell>
-                                <Typography variant="subtitle2">{row.symbol}</Typography>
-                                <Typography variant="caption">{row.name}</Typography>
-                            </StickyTableCell>
-                            <TableCell>
-                                {currencyFormatter.format(row.quote.USD.price)}
-                            </TableCell>
-                            <TableCell>
-                                {largeCurrencyFormatter(row.quote.USD.market_cap)}
-                            </TableCell>
-                            <TableCell>
-                                {largeCurrencyFormatter(row.quote.USD.volume_24h)}
-                            </TableCell>
-                            <TableCell>
-                                {percentFormatter.format(row.quote.USD.percent_change_1h / 100)}
-                            </TableCell>
-                            <TableCell>
-                                {percentFormatter.format(row.quote.USD.percent_change_24h / 100)}
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                    {emptyRows > 0 && (
-                        <TableRow>
-                            <TableCell colSpan={7} />
-                        </TableRow>
+            <TableContainer className={classes.container}>
+                <Table size="small" aria-label="a dense table">
+                    {headers && (
+                        <TableHead>
+                            <TableRow>
+                                {headers.map((header: string, index: number) => {
+                                    if (index === 0) {
+                                        return (
+                                            <TableCell key={index}>
+                                                {header}
+                                            </TableCell>
+                                        )
+                                    } else if (index === 1) {
+                                        return (
+                                            <StickyTableCell key={index}>
+                                                {header}
+                                            </StickyTableCell>
+                                        );
+                                    } else {
+                                        return (
+                                            <TableCell align="right" key={index}>
+                                                {header}
+                                            </TableCell>
+                                        );
+                                    }
+                                })}
+                            </TableRow>
+                        </TableHead>
                     )}
-                </TableBody>
-            </Table>
-        </TableContainer>
-        <div className={classes.container}>
-            <TablePaginationActions
-             count={listings.length}
-             rowsPerPage={rowsPerPage}
-             page={page}
-             onChangePage={handleChangePage}
-            />
-        </div>
+                    <TableBody>
+                        {(rowsPerPage > 0
+                            ? listings.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : listings).map((row: any) => (
+                                <TableRow key={row.id}>
+                                    <TableCell>
+                                        {row.cmc_rank}
+                                    </TableCell>
+                                    <StickyTableCell>
+                                        <Typography variant="subtitle2">
+                                            {row.symbol}
+                                        </Typography>
+                                        <Typography variant="caption">
+                                            {row.name}
+                                        </Typography>
+                                    </StickyTableCell>
+                                    <TableCell align="right">
+                                        {currencyFormatter.format(row.quote.USD.price)}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        {largeCurrencyFormatter(row.quote.USD.market_cap)}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        {largeCurrencyFormatter(row.quote.USD.volume_24h)}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        {percentFormatter.format(row.quote.USD.percent_change_1h / 100)}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        {percentFormatter.format(row.quote.USD.percent_change_24h / 100)}
+                                    </TableCell>
+                                </TableRow>
+                            )
+                        )}
+                        {emptyRows > 0 && (
+                            <TableRow>
+                                <TableCell colSpan={7} />
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <div className={classes.container}>
+                <TablePaginationActions
+                count={listings.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                />
+            </div>
         </>
     );
 };
