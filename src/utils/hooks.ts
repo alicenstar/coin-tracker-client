df559557c88556b5a6b1adcc434ec69eb0bdbb15
Star <67430892+alicenstar@users.noreply.github.com>
2021-04-29 16:18:11 -0500

    Rename file

---
+++ src/utils/hooks.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+export const useInterval = (callback: any, delay: any) => {
+    const savedCallback = React.useRef<any>();
+  
+    React.useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+  
+    React.useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+};
